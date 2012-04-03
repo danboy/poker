57c784af945c5a5da777aaa6d62cdcd0823cb676
Dan Nawara <dnawara@groupon.com>
2012-04-03 09:14:49 -0500

    start on any slide

---
+++ spec/controllers/projects_spec.js
@@ -0,0 +1,7 @@
+var Projects = require( __dirname + '/../../app/controllers/projects_controller.js')
+  , should   = require('should');
+
+describe('Project', function(){
+  it('should retrive a list of users projects when a key is present',function(){
+  });
+});
